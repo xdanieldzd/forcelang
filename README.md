cf3b0edd84a514f3f3ff2e70631d0f4824ca495b
xdanieldzd <xdanieldzd@googlemail.com>
2018-07-31 19:22:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+forcelang
+=========
+
+A taiHEN plugin for overriding an application's language, if queried by `sceAppUtilSystemParamGetInt` and `SCE_SYSTEM_PARAM_ID_LANG`.
+
+Installation
+------------
+
+Copy `forcelang.suprx` to `ur0:/tai`, then add the plugin to the `config.txt` section of each application you want to use it with.
+
+```# Example for Digimon Story: Cyber Sleuth (European PSN)
+*PCSB00861
+ur0:tai/forcelang.suprx
+```
+
+Configuration
+-------------
+
+When starting an application with the plugin enabled, the plugin will look for, and if necessary create, a configuration file at `ur0:/data/forcelang/[title ID].txt`, ex. `PCSB00861.txt`.
+
+This file contains a single number, corresponding to the Vita system's `SceSystemParamLang` enum. Thus, valid values are:
+
+* 0: Japanese
+* 1: American English
+* 2: French
+* 3: Spanish
+* 4: German
+* 5: Italian
+* 6: Dutch
+* 7: Portugal Portuguese
+* 8: Russian
+* 9: Korean
+* 10: Traditional Chinese
+* 11: Simplified Chinese
+* 12: Finnish
+* 13: Swedish
+* 14: Danish
+* 15: Norwegian
+* 16: Polish
+* 17: Brazil Portuguese
+* 18: British English
+* 19: Turkish
+
+Do note that the application might not support the language requested, or it might not use the function patched by this plugin to determine the language. If an application supports switching languages ex. via its options menu, use that instead.
---
+++ forcelang.c
@@ -0,0 +1,132 @@
+ /*
+    forcelang - language override plugin
+    Written in July 2018 by xdaniel - github.com/xdanieldzd
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+#include <psp2/kernel/modulemgr.h>
+#include <psp2/io/fcntl.h>
+#include <psp2/appmgr.h>
+#include <psp2/system_param.h>
+
+#include <kuio.h>
+#include <taihen.h>
+
+#define DATA_DIRECTORY "ur0:/data/forcelang"
+
+static tai_hook_ref_t sysParamGet_hookRef;
+static SceUID taiPatchRef;
+
+char * appTitleId;
+char * filename;
+char * buffer;
+
+int language = SCE_SYSTEM_PARAM_LANG_ENGLISH_US;
+
+int sceAppUtilSystemParamGetInt_patched(unsigned int paramId, int *value)
+{
+    if (paramId == SCE_SYSTEM_PARAM_ID_LANG)
+    {
+        *value = language;
+        return 0;
+    }
+
+    return TAI_CONTINUE(int, sysParamGet_hookRef, paramId, value);
+}
+
+void _start() __attribute__ ((weak, alias ("module_start")));
+
+void fetchInformation()
+{
+    // Initialize SceAppUtil & get system language
+    SceAppUtilInitParam initParam;
+    memset(&initParam, 0, sizeof(SceAppUtilInitParam));
+    SceAppUtilBootParam bootParam;
+    memset(&bootParam, 0, sizeof(SceAppUtilBootParam));
+    sceAppUtilInit(&initParam, &bootParam);
+    sceAppUtilSystemParamGetInt(SCE_SYSTEM_PARAM_ID_LANG, &language);
+    sceAppUtilShutdown();
+
+    // Get application's title ID
+    appTitleId = malloc(sizeof(char) * 12);
+    sceAppMgrAppParamGetString(0, 12, appTitleId, 256);
+}
+
+void test()
+{
+    SceUID fd;
+    kuIoOpen(DATA_DIRECTORY "/test.txt", SCE_O_WRONLY | SCE_O_CREAT | SCE_O_TRUNC, &fd);
+    if(fd >= 0)
+    {
+        kuIoWrite(fd, appTitleId, sizeof(char) * 12);
+        kuIoClose(fd);
+    }
+}
+
+void readConfig()
+{
+    // Allocate buffer
+    buffer = malloc(sizeof(char) * 128);
+    memset(buffer, 0, 128);
+
+    // Try to open config file, also create directory if necessary
+    kuIoMkdir(DATA_DIRECTORY);
+    SceUID fd;
+    filename = malloc(sizeof(char) * 128);
+    sprintf(filename, DATA_DIRECTORY "/%s.txt", appTitleId);
+    kuIoOpen(filename, SCE_O_RDONLY, &fd);
+
+    if (fd >= 0)
+    {
+        // Config file exists, read the value
+        kuIoRead(fd, buffer, 128);
+        kuIoClose(fd);
+
+        sscanf(buffer, "%d", &language);
+    }
+    else
+    {
+        // Config file doesn't exist, try to create it
+        kuIoOpen(filename, SCE_O_WRONLY | SCE_O_CREAT | SCE_O_TRUNC, &fd);
+        if (fd >= 0)
+        {
+            // Config file created, write the system language as default
+            sprintf(buffer, "%d", language);
+
+            kuIoWrite(fd, buffer, sizeof(char));
+            kuIoClose(fd);
+        }
+    }
+}
+
+int module_start(SceSize argc, const void *args)
+{
+    // Do stuff!
+    fetchInformation();
+    //test();
+    readConfig();
+
+    // Patch sceAppUtilSystemParamGetInt
+    taiPatchRef = taiHookFunctionImport(&sysParamGet_hookRef,
+                                         TAI_MAIN_MODULE,
+                                         0x48E01D74,    // SceAppUtil
+                                         0x5DFB9CA0,    // sceAppUtilSystemParamGetInt
+                                         sceAppUtilSystemParamGetInt_patched);
+
+    return SCE_KERNEL_START_SUCCESS;
+}
+
+int module_stop(SceSize argc, const void *args)
+{
+    // Release and free everything
+    taiHookRelease(taiPatchRef, sysParamGet_hookRef);
+
+    free(appTitleId);
+    free(filename);
+    free(buffer);
+
+    return SCE_KERNEL_STOP_SUCCESS;
+}
